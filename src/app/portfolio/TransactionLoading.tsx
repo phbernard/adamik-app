6a08071e01d2252253c70d730e051910cbdcccb3
fabrice-adamik <159912605+fabrice-adamik@users.noreply.github.com>
2024-06-25 17:03:21 +0200

    Make sure correct logo color is applied based on theme

--- src/app/portfolio/TransactionLoading.tsx
+++ src/app/portfolio/TransactionLoading.tsx
@@ -1,13 +1,25 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useTheme } from 'next-themes';
 
 type TransactionLoadingProps = {
   onNextStep: () => void;
 };
 
 export const TransactionLoading = ({ onNextStep }: TransactionLoadingProps) => {
-  setTimeout(() => {
-    onNextStep();
-  }, 3000);
+  const [mounted, setMounted] = useState(false);
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+    setTimeout(() => {
+      onNextStep();
+    }, 3000);
+  }, [onNextStep]);
+
+  if (!mounted) return null; // Ensures theme is mounted before rendering
 
   return (
     <div>
@@ -15,15 +27,17 @@ export const TransactionLoading = ({ onNextStep }: TransactionLoadingProps) => {
         <Image
           className="animate-spin"
           alt="Adamik logo"
-          src="/adamik_symbol.svg"
+          src={
+            theme === "light"
+              ? "/adamik_symbol_blue.svg"
+              : "/adamik_symbol_white.svg"
+          }
           width={64}
           height={64}
         />
       </div>
-
       <div className="p-4">
-        Your transaction request has been sent to the Adamik API, and processing
-        is currently underway!
+        Your transaction request has been sent to the Adamik API, and processing is currently underway!
       </div>
     </div>
   );
