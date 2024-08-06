957e0fbe42cbd107ace005e43e1ede883b5131ae
fabrice-adamik <159912605+fabrice-adamik@users.noreply.github.com>
2024-08-06 13:00:02 +0200

    minor UI improvement

--- src/app/supported-chains/page.tsx
+++ src/app/supported-chains/page.tsx
@@ -167,7 +167,7 @@ export default function SupportedChains() {
             {isLoading ? (
               <Loader2 className="animate-spin" />
             ) : (
-              <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-4">
+              <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-4 mt-4">
                 {filteredChains?.map((chain) => {
                   const isComingSoon = comingSoonIds.includes(chain.id);
 
